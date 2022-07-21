182a089a377521e7cd58d7e49e1356f0923076b8
Preyash <uiux5@varaluae.com>
2022-07-21 18:30:03 +0530

    mask removed

--- index.js
+++ index.js
@@ -43,18 +43,6 @@ document.querySelector('.more-button').addEventListener('click', function () {
 	document.querySelector('.list-container').classList.toggle('menu-active');
 });
 
-// setInterval(function () {
-// 	const show = document.querySelector('span[data-show]')
-// 	const next = show.nextElementSibling || document.querySelector('span:first-child')
-// 	const up = document.querySelector('span[data-up]')
-// 	if (up) {
-// 		up.removeAttribute('data-up')
-// 	}
-// 	show.removeAttribute('data-show')
-// 	show.setAttribute('data-up', '')
-// 	next.setAttribute('data-show', '')
-// }, 2000)
-
 $(document).ready(function () {
 	var progressPath = document.querySelector('.progress-wrap path');
 	var pathLength = progressPath.getTotalLength();
