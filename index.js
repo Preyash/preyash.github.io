f6302131d659b8354845fd180d641811c1640ee5
Preyash <uiux5@varaluae.com>
2022-07-21 18:25:48 +0530

    all the things are done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const links = Array.from(document.querySelectorAll('.links'))
-
-links.map((i, j) =>
+links.map((i) =>
 	i.addEventListener("click", function () {
 		[].forEach.call(document.querySelectorAll(".active"), (i) => {
 			i.classList.remove('active');
-			i.children[0].src = i.children[0].src.replace('_filled', '');
+			console.log(i)
+			i.children[0].src = i.children[0]?.src?.replace('_filled', '');
 		});
 		i.className += " active";
 		i.children[0].src = i.children[0].src.split('.png')[0] + '_filled.png';
@@ -15,74 +15,45 @@ window.onload = () => {
 	Array.from(document.querySelectorAll('.slider2 .slick-list img')).map(i => i.classList.add('img-fluid'))
 }
 
-const links_data = document.querySelector('.links_data')
 
-Array.from(links_data.children).map((i) =>
-	i.addEventListener("mouseenter", function () {
-		i.children[0].src = i.children[0].src.replace('-systems-filled', '')
-	})
-)
-
-Array.from(links_data.children).map((i) =>
-	i.addEventListener("mouseleave", function () {
-		let src = i.children[0].src
-		let index = src.indexOf('fluency') + 'fluency'.length
-		let newSrc = src.slice(0, index) + "-systems-filled" + src.slice(index);
-		i.children[0].src = newSrc;
-	})
-)
-
-let responsive = [
-	{
-		breakpoint: 768,
-		settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		}
-	},
-	{
-		breakpoint: 480,
-		settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		}
-	}
-]
-
-let slidesToShow = $('.slider') ? 2 : 1
 $(document).ready(function () {
-	$('.slider').slick({
-		infinite: true,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		prevArrow: "<img class='slick-prev slick-arrow' src='./img/left.png' />",
-		nextArrow: "<img class='slick-next slick-arrow' src='./img/left.png' />",
-		responsive: responsive
-	});
-	$('.slider2').slick({
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: "<img class='slick-prev slick-arrow' src='./img/left.png' />",
-		nextArrow: "<img class='slick-next slick-arrow' src='./img/left.png' />",
-		responsive: responsive
-	});
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		nav: true,
+		margin: 10,
+		responsiveClass: true,
+		navText: ["<img class='slick-prev' src='./img/left.png' />", "<img class='slick-next' src='./img/left.png'/>"],
+		responsive: {
+			0: {
+				items: 1,
+				nav: true,
+			},
+			600: {
+				items: 1,
+				nav: true,
+			},
+			1000: {
+				items: 1,
+			}
+		}
+	})
 });
 
+document.querySelector('.more-button').addEventListener('click', function () {
+	document.querySelector('.list-container').classList.toggle('menu-active');
+});
 
 // setInterval(function () {
-//   const show = document.querySelector('span[data-show]')
-//   const next = show.nextElementSibling || document.querySelector('span:first-child')
-//   const up = document.querySelector('span[data-up]')
-//   if (up) {
-//     up.removeAttribute('data-up')
-//   }
-//   show.removeAttribute('data-show')
-//   show.setAttribute('data-up', '')
-//   next.setAttribute('data-show', '')
+// 	const show = document.querySelector('span[data-show]')
+// 	const next = show.nextElementSibling || document.querySelector('span:first-child')
+// 	const up = document.querySelector('span[data-up]')
+// 	if (up) {
+// 		up.removeAttribute('data-up')
+// 	}
+// 	show.removeAttribute('data-show')
+// 	show.setAttribute('data-up', '')
+// 	next.setAttribute('data-show', '')
 // }, 2000)
-
-
 
 $(document).ready(function () {
 	var progressPath = document.querySelector('.progress-wrap path');
